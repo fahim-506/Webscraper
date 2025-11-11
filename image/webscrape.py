20efbd9c0ff78258c31cfda24910102c675ca9ef
Hafizmuhammedk <hafizmuhammed1019@gmail.com>
2025-11-11 11:54:17 +0000

    image page updated

--- image/webscrape.py
+++ image/webscrape.py
@@ -4,6 +4,7 @@ import os
 from urllib.parse import urljoin
 from firecrawl import Firecrawl
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
@@ -68,3 +69,32 @@ def get_images(url: str):
             deduped.append(u)
 
     return deduped
+
+
+st.set_page_config(page_title="Firecrawl scraper", layout="wide")
+st.title("Firecrawl Image Viewer")
+st.write("Paste website link below to scrape image")
+
+url = st.text_input("Enter a website URL", placeholder="https://example.com")
+
+
+if url:
+    with st.spinner("Finding images..."):
+        images = get_images(url)
+
+    if not images:
+        st.warning("No images found.")
+    else:
+        st.success(f"Found {len(images)} images")
+
+        # allow user to control image width
+        img_width = st.slider(
+            "adjust the width (pixels)",
+            min_value=100, max_value=1200,
+            value=400
+        )
+
+        cols = st.columns(3)
+        for i, img in enumerate(images):
+            with cols[i % 3]:
+                st.image(img, width=img_width)
